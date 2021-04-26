1761aca73251839cbbf12200fd073bc68a42964a
James Winchester <wildrover254@gmail.com>
2021-04-25 22:18:18 -0400

    Major update to most files

--- app.js
+++ app.js
@@ -9,19 +9,28 @@ app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
+/**
+*  Renders the homepage
+*/
 app.get('/', (req, res) => {
     res.render('index.pug', { projects });
-    console.log('app running');
 });
 
-app.get('/about', (req, res) => {
+/**
+ * Renders the About page
+ */
+app.get('/about', (req, res, next) => {
     res.render('about.pug');
 });
 
+/**
+ * Renders the project pages and creates a 404 if an invalid
+ * project id is in the param
+ */
 app.get('/project/:id', (req,res,next) => {
     const { id } = req.params;
 
-    if (id <= projects.length) {
+    if (projects[id]) {
         const { project_name } = projects[id];
         const { description } = projects[id];
         const { live_link } = projects[id];
@@ -36,20 +45,35 @@ app.get('/project/:id', (req,res,next) => {
     } else {
         const err = new Error('Not Found!');
         err.status = 404;
+        console.log(`There has been a ${err.status} error`)
         next(err);
     }
 });
 
-/*app.use((req, res, next) => {
-    const err = new Error('Not Found!');
+/**
+ * Creates a 404 error and renders the 'page-not-found' template
+ */
+app.use((req, res, next) => {
+    const err = new Error();
     err.status = 404;
-    next(err);
-});*/
+    err.message = 'This page does not exist';
+    console.log(`There has been a ${err.status} error`)
+    res.render('page-not-found', { err });
+});
 
+/**
+ * Global error catch. Renders 'page-not-found' template for a 404.
+ * Renders the 'error' template for other errors.
+ */
 app.use((err, req, res, next) => {
-    res.locals.error = err;
-    res.status(err.status);
-    res.render('page-not-found', err);
+   if (err.status === 404) {
+       console.log(`There has been a ${err.status} error`)
+       res.status(404).render('page-not-found', { err });
+   } else {
+       err.message = err.message || `There has been a server error.`;
+       console.log(`There has been a ${err.status} error`)
+       res.status(err.status || 500).render('error', { err });
+   }
 });
 
 app.listen(3000, () => {
--- readMe.txt
+++ readMe.txt
@@ -1,3 +1,8 @@
 Added styles for the error pages
 Added a spooky ghost
 ghost.svg source: https://www.opensymbols.org/symbols/mulberry/ghost-50b6e8eb?id=14139
+
+Changed sidebar color and added a box shadow to the profile picture. Adjusted
+color of href link to match the new color.
+Slightly thickened the border line at the bottom of the intro paragraph 
+and added a duplicate with padding to the top.
